5ac744aba1f0360a3f47bb2cdd3bf4975ba9cb5e
kim gisu <kgs9843@naver.com>
2024-09-20 03:44:36 +0900

    finsh-myinfo

--- src/page/Mypage/Tabs.jsx
+++ src/page/Mypage/Tabs.jsx
@@ -10,19 +10,23 @@ const Tabs = ({ children }) => {
 
   return (
     <div className="tabs">
-      {children.map((child) => (
-        <Tab
-          key={child.props.label}
-          label={child.props.label}
-          onClick={handleTabClick}
-          activeTab={activeTab}
-        />
-      ))}
+      <div id='tabs_divider'>
+        {children.map((child) => (
+          <Tab
+            key={child.props.label}
+            label={child.props.label}
+            onClick={handleTabClick}
+            activeTab={activeTab}
+          />
+        ))}
+      </div>
+
       <div className="tab-content">
         {children.map((child) =>
           child.props.label === activeTab ? child.props.children : null
         )}
       </div>
+
     </div>
   );
 };
